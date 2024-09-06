f5fcade518dd906064e39862345c11fd8c61260b
zzarr <azharsyah.curug@gmail.com>
2024-09-06 16:00:07 +0700

    add slicing page

---
+++ resources/views/admin/componen/app.blade.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <!-- BEGIN GLOBAL MANDATORY STYLES -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:400,600,700" rel="stylesheet">
+    <link href="{{ asset('demo5/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('demo5/assets/css/plugins.css') }}" rel="stylesheet" type="text/css" />
+    <!-- END GLOBAL MANDATORY STYLES -->
+
+    <!-- BEGIN PAGE LEVEL PLUGINS/CUSTOM STYLES -->
+
+    <style>
+        /*
+             The below code is for DEMO purpose --- Use it if you are using this demo otherwise Remove it
+         */
+        .navbar .navbar-item.navbar-dropdown {
+            margin-left: auto;
+        }
+
+        .layout-px-spacing {
+            min-height: calc(100vh - 96px) !important;
+        }
+    </style>
+</head>
+
+<body class="sidebar-noneoverflow">
+    <!--  BEGIN NAVBAR  -->
+    @include('admin.componen.navbar')
+    <!--  END NAVBAR  -->
+
+    <!--  BEGIN MAIN CONTAINER  -->
+    <div class="main-container" id="container">
+
+        <div class="overlay"></div>
+        <div class="cs-overlay"></div>
+        <div class="search-overlay"></div>
+        <!-- START SIDEBAR -->
+        @include('admin.componen.sidebar')
+        <!-- END SIDEBAR -->
+
+        <!--  BEGIN CONTENT AREA  -->
+        <div id="content" class="main-content">
+            <div class="layout-px-spacing">
+                @yield('content')
+            </div>
+            <div class="footer-wrapper">
+                <div class="footer-section f-section-1">
+                    <p class="">Copyright © 2020 <a target="_blank"
+                            href="https://designreset.com">DesignReset</a>, All rights reserved.</p>
+                </div>
+                <div class="footer-section f-section-2">
+                    <p class="">Coded with <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                            viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                            stroke-linecap="round" stroke-linejoin="round" class="feather feather-heart">
+                            <path
+                                d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z">
+                            </path>
+                        </svg></p>
+                </div>
+            </div>
+        </div>
+        <!--  END CONTENT AREA  -->
+
+        <!-- BEGIN GLOBAL MANDATORY SCRIPTS -->
+        <script src="{{ asset('demo5/assets/js/libs/jquery-3.1.1.min.js') }}"></script>
+        <script src="{{ asset('demo5/bootstrap/js/popper.min.js') }}"></script>
+        <script src="{{ asset('demo5/bootstrap/js/bootstrap.min.js') }}"></script>
+        <script src="{{ asset('demo5/plugins/perfect-scrollbar/perfect-scrollbar.min.js') }}"></script>
+        <script src="{{ asset('demo5/assets/js/app.js') }}"></script>
+
+        <script>
+            $(document).ready(function() {
+                App.init();
+            });
+        </script>
+        <script src="{{ asset('demo5/assets/js/custom.js') }}"></script>
+        <!-- END GLOBAL MANDATORY SCRIPTS -->
+
+
+    </div>
+
+
+
+</body>
+
+</html>
